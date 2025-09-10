05022361665bd84831aa5f962f78d618337d0215
Jay Timbol <jlc.timbol@gmail.com>
2025-09-09 23:32:36 -0700

    accounting admit rates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+//
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { fetchAdmissions } from './utils/api';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchAdmissions().then(setData);
+  }, []);
+
+  const major = data[0].major;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>{major}</h1>
+    <ul>
+      {data.map(row => {
+        const admitRate = 
+        row.num_admits && row.num_applicants
+            ? ((row.num_admits / row.num_applicants) * 100).toFixed(1)
+            : 'N/A'; // if missing data
+
+            return (
+              <li key={row.id}>
+                {row.year}: {admitRate}%
+              </li>
+            )
+      })}
+    </ul>
+  </div>
   );
 }
 
---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+//get data from supabase
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://asvvvzozokccidthnzmj.supabase.co';
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+export const client = createClient(supabaseUrl, supabaseKey);
+
+export async function fetchAdmissions() {
+     const { data, error } = await client
+    .from('admission_data')
+    .select('*');
+
+    if (error) {
+        console.error('Error fetching admissions:', error);
+        return [];
+    }
+    else {
+        console.log(data);
+    }
+    return data;
+}
